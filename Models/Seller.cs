08407d2380323ab0c9e7779c5ab61db9b4778501
Gustavo Goncalves <gustavo.silveira.goncalves@gmail.com>
2022-06-23 14:37:33 -0300

    Projeto 1
    Inicio do Projeto

---
+++ Models/Seller.cs
@@ -0,0 +1,20 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+        }
+    }
+}
